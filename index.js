dad369c50cc70a3aa2b9e6566ae12dea75931130
mateusbernardes23 <mateusbernardes23@hotmail.com>
2023-04-12 23:28:45 -0300

    Create connection in mysql

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão com sucesso!");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
 
-//Estou dizendo para o express usar o EJS como View engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //Estou dizendo para o express usar o EJS como View engine
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -11,6 +24,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " descricao " + descricao);
 });
 
 app.listen(3000, () => {
